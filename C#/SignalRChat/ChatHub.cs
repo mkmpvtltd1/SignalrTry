01548ba4b264bcb1fc04243e95b35d7cefe40f76
Manoj Mahato <mmahato@braindigit.com>
2016-03-17 17:48:29 +0545

    SignalR Try Init

---
+++ C#/SignalRChat/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message, string asdas)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message, asdas);
+        }
+        public void Cordinate(string x, string y)
+        {
+            Clients.All.draw(x, y);
+        }
+    }
+}
